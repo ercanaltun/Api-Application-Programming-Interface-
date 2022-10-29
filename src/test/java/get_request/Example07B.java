00870430886991516737fc35ed8016d8e2986607
Ercan ALTUN <salsel0147@gmail.com>
2022-10-29 14:40:05 +0300

    29/10.

--- src/test/java/get_request/Example07B.java
+++ src/test/java/get_request/Example07B.java
@@ -32,40 +32,43 @@ public class Example07B extends JsonplaceholderBaseUrl {
     public void test07B() {
 
         //1.Step:Set the Url
-        spec.pathParam("first","todos");
+        spec.pathParam("first", "todos");
 
         //2.Step:Set the expected data
 
         //3.Step:Send the Request and Get the Response
-        Response response=given().spec(spec).when().get("/{first}");
+        Response response = given().spec(spec).when().get("/{first}");
 
         //4.Step:Do Assertion
         //1)Status code is 200 == > Status kodu 200 olmali
         response.then().assertThat().statusCode(200);
-        assertEquals(200,response.statusCode());
+        assertEquals(200, response.statusCode());
 
         //2)Print all ids greater than 190 on the console ==> id si 190 dan buyuk olanlari konsola yazdirin
-        JsonPath json=response.jsonPath();
-        List<Integer>idler=json.getList("findAll{it.id>190}.id");
+        JsonPath json = response.jsonPath();
+        List<Integer> idler = json.getList("findAll{it.id>190}.id");
         System.out.println("idler = " + idler);
 
         //Assert that there are 10 ids greater than 190 == > 10 tane id nin 190 dan buyuk oldugunu dogrulayin
-        assertEquals(10,idler.size());
+        assertEquals(10, idler.size());
 
         //3)Print all userIds whose ids are less than 5 on the console ==> id si 5 den kucuk olan tum userid lerini konsolunu yazdirin
-        List<Integer>userIdler=json.getList("findAll{it.id<5}.userId");
+        List<Integer> userIdler = json.getList("findAll{it.id<5}.userId");
         System.out.println("userIdler = " + userIdler);
 
         //Assert that the number of userIds whose ids are less than 5 is 4 ==> id si 5 den kucuk olan 4 tane userId oldugunu dogrulayin
-        assertEquals(4,userIdler.size());
+        assertEquals(4, userIdler.size());
 
         //4)Print all titles whose ids are less than 5 ==> ıd si 5 den kucuk olan tum basliklari yazdirin
-        List<String>titles=json.getList("findAll{it.id<5}.title");
+        List<String> titles = json.getList("findAll{it.id<5}.title");
         System.out.println("titles = " + titles);
 
         //Assert that "delectus aut autem" is one of the titles whose id is less than 5 ==> id si 5 den kucuk olan datalarin birinin
         //basliginin "delectus aut autem" icerdigini dogrulayin
         assertTrue(titles.contains("delectus aut autem"));
 
+
     }
+
+
 }
